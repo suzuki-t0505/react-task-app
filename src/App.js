aa3de1d41bbb823c544a96294f21b877ccd9657c
suzuki-t0505 <takumi.s@thewaggle.co.jp>
2024-04-22 10:20:18 +0900

    taskアプリの作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([{id: 1, text: 'test1'}, {id: 2, text: 'test2'}]);
+  const [taskText, setTaskText] = useState('');
+
+  const changeTaskText = (text) => {
+    setTaskText(text);
+  }
+
+  const addTask = () => {
+    if (taskText === '')
+    {
+      alert('Please enter your task.');
+      return;
+    }
+
+    setTasks((prevTasks) => [...prevTasks, {id: prevTasks.length + 1, text: taskText}]);
+    setTaskText('');
+  }
+
+  const deleteTask = (id) => {
+    if (window.confirm('Delete your task?'))
+    {
+      setTasks(tasks.filter((task) => task.id !== id));
+    }
+
+    return;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Task App</h1>
+      <div>
+        <input
+          type="text"
+          value={taskText}
+          onChange={(e) => changeTaskText(e.target.value)}
+          placeholder='Please enter your task'
+        />
+        <button type="button" onClick={() => addTask()}>Add</button>
+      </div>
+      <ul>
+        {
+          tasks.map((task) => (
+            <li>
+              {task.text}
+              <button onClick={() => deleteTask(task.id)}>Delete</button>
+            </li>
+          ))
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
